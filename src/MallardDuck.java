bb35dc7002bb656d39cf0fbcff3f0482b55ea7dd
unknown <Stud_5@5_07.efdnew.isuct.ru>
2014-09-11 14:57:39 +0400

    first commit

---
+++ src/MallardDuck.java
@@ -0,0 +1,11 @@
+class MallardDuck extends Duck {
+
+    public MallardDuck() {
+    }
+
+    @Override
+    public void display() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
