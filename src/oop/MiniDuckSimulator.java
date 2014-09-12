fbbf8f1d04076e5c073db0a0b4feec4952146d68
Ermakova <nadegda.ermakova@mail.ru>
2014-09-12 17:01:12 +0400

    .java

---
+++ src/oop/MiniDuckSimulator.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop;
+
+/**
+ *
+ * @author -
+ */
+public class MiniDuckSimulator {
+    public static void main(String[] args) {
+        Duck mallard=new MallardDuck();
+        mallard.performQuack();
+        mallard.performFly();
+    }
+}
---
+++ src/oop/MuteQuack.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop;
+
+/**
+ *
+ * @author -
+ */
+public class MuteQuack {
+    public void quack() {
+        System.out.println("<<Silence>>");
+    }
+}
