92c8bab61f1dc41a6c1e3c5082457a6b03e3e1fb
wolfekon <wolfekon-m@mail.ru>
2014-09-20 18:39:48 +0400

    Revert "Revert "duckkk""
    This reverts commit e28d42373c51cc2560eee23584d785f96d256f3b.

---
+++ MallardDuck.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Stud_5
+ */
+class MallardDuck extends Duck {
+
+    public MallardDuck() {
+    }
+
+    @Override
+    public void display() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
