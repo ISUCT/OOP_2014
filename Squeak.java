6b599372393675e0b7f813f46207f5af6f948753
unknown <Stud_5@5_05.efdnew.isuct.ru>
2014-09-25 18:39:22 +0900

    Duck

---
+++ Squeak.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Stud_5
+ */
+
+     public class Squeak implements QuackBehavior {
+    public void quack() {
+        System.out.println("Squeak");
+    }
+
+    @Override
+    public void guack() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
+    
