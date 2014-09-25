929cfd9aaa6608b580e26898f0d8bdb0656985f6
unknown <stud_5@5_02.efdnew.isuct.ru>
2014-09-25 14:50:28 +0400

    New Structure

---
+++ src/Duck/FlyWithWings.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Duck;
+
+/**
+ *
+ * @author stud_5
+ */
+public class FlyWithWings implements FlyBehavior{
+    @Override
+    public void fly(){
+        System.out.println("I'm flying!");
+    }
+}
