073933c2f5c5dee8e74a20649a6276882e65eaca
AlexCPU7 <AlexCPU7@mail.ru>
2014-10-08 18:41:32 +0400

    duck

---
+++ Squeak.java
@@ -0,0 +1,18 @@
+/*
+ * Пищат
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Duck;
+
+/**
+ *
+ * @author stud_5
+ */
+public class Squeak implements QuackBehavior{
+    @Override
+    public void quack() {
+        System.out.println("Squeak");
+    }   
+}
