b4a6dfd4aa105aac7516fde789962bf1702019c4
AlexCPU7 <AlexCPU7@mail.ru>
2014-10-13 23:48:25 +0400

    Regex

---
+++ RegexTest/src/regextest/RegexTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regextest;
+
+/**
+ *
+ * @author l
+ */
+public class RegexTest {
+
+    public static void main(String[] args) {
+    }
+    
+    public static String regEx = "";
+    
+}
---
+++ RegexTest/test/regextest/RegexTestTest.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regextest;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 1
+ */
+public class RegexTestTest {
+
+    private static String REGEX = "(?<Letter>[А-Яа-я]{1})\\W*(?<Figures>\\d{3})\\W*(?<Letters>[А-Ба-б]{2})\\D*(?<Region>\\d{2})";
+    private static String number1 = "а000аб37";
+    private static String number2 = "а 000 аа 37";
+    private static String number3 = "а000аа регион 37";
+    private static String number4 = "а-000-аа-37";
+    private static String number5 = "а-000-аа 37";
+    private Pattern myPattern = Pattern.compile(REGEX);
+
+    @Test
+    public void testMain1() {
+        System.out.println("1 test");
+        Matcher myMatch = myPattern.matcher(number1);
+        if (myMatch.find()) {
+            for (int i = 1; i <= myMatch.groupCount(); i++) {
+                System.out.println(myMatch.group(i));
+            }
+            assertEquals("а", myMatch.group(1));
+            assertEquals("000", myMatch.group(2));
+            assertEquals("аб", myMatch.group(3));
+            assertEquals("37", myMatch.group(4));
+        } else {
+            fail();
+        }
+    }
+    
+    @Test
+    public void testMain2() {
+        System.out.println("2 test");
+        Matcher myMatch = myPattern.matcher(number2);
+        if (myMatch.find()) {
+            for (int i = 1; i <= myMatch.groupCount(); i++) {
+                System.out.println(myMatch.group(i));
+            }
+            assertEquals("а", myMatch.group(1));
+            assertEquals("000", myMatch.group(2));
+            assertEquals("аа", myMatch.group(3));
+            assertEquals("37", myMatch.group(4));
+        } else {
+            fail();
+        }     
+    }
+    
+    @Test
+    public void testMain3() {
+        System.out.println("3 test");
+        Matcher myMatch = myPattern.matcher(number3);
+        if (myMatch.find()) {
+            for (int i = 1; i <= myMatch.groupCount(); i++) {
+                System.out.println(myMatch.group(i));
+            }
+            assertEquals("а", myMatch.group(1));
+            assertEquals("000", myMatch.group(2));
+            assertEquals("аа", myMatch.group(3));
+            assertEquals("37", myMatch.group(4));
+        } else {
+            fail();
+        }
+    }
+    
+    @Test
+    public void testMain4() {
+        System.out.println("4 test");
+        Matcher myMatch = myPattern.matcher(number4);
+        if (myMatch.find()) {
+            for (int i = 1; i <= myMatch.groupCount(); i++) {
+                System.out.println(myMatch.group(i));
+            }
+            assertEquals("а", myMatch.group(1));
+            assertEquals("000", myMatch.group(2));
+            assertEquals("аа", myMatch.group(3));
+            assertEquals("37", myMatch.group(4));
+        } else {
+            fail();
+        }
+    }
+    
+       @Test
+    public void testMain5() {
+        System.out.println("5 test");
+        Matcher myMatch = myPattern.matcher(number5);
+        if (myMatch.find()) {
+            for (int i = 1; i <= myMatch.groupCount(); i++) {
+                System.out.println(myMatch.group(i));
+            }
+            assertEquals("а", myMatch.group(1));
+            assertEquals("000", myMatch.group(2));
+            assertEquals("аа", myMatch.group(3));
+            assertEquals("37", myMatch.group(4));
+        } else {
+            fail();
+        }   
+    }
+}
