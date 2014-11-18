2538121939e14ae0ba15bd5a7002fac7a13e60f5
GoldSky37 <1-2-93@bk.ru>
2014-11-18 19:20:43 +0300

    Pogodnaia stancia

---
+++ Forecastdisplay.java
@@ -0,0 +1,17 @@
+package weather.station;
+public class ForecastDisplay implements Observer,DisplayElement{
+private float temperature;
+private float humidity;
+public void update (float temperature, float humidity, float pressure){
+        this.temperature = temperature;
+        this.humidity = humidity;
+        display();
+        
+    }  
+public void display (){
+System.out.println("Выдать прогноз");
+ 
+    }
+
+
+}   
